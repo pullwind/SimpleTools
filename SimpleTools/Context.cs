654021259620df5ea6a12b192de552f94c164392
whitewolf <whitewolf_asd@outlook.com>
2022-11-10 21:11:18 +0800

    Add project files.

---
+++ SimpleTools/Context.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleTools
+{
+    class Context :DbContext
+    {
+        public Context() : base ("name=c2")
+        {
+
+        }
+
+        public DbSet<ThePC> thePCs { get; set; }
+
+    }
+}
